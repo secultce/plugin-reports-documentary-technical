dcfccb1c23642d56b0548864ab716afac4186e3e
wylder.menezes <wylder.menezes@grupoanga.com>
2022-06-28 13:52:53 -0300

    fix: ajusta table html para gerar relatorio em xls

--- lib/PhpSpreadsheetLib.php
+++ lib/PhpSpreadsheetLib.php
@@ -13,16 +13,17 @@ class PhpSpreadsheetLib
         $spreadsheet = new Spreadsheet();
         $reader = new Html();
         ob_start();
-        require __DIR__ . './../layouts/parts/contents/DocumentalPDF.php';
+        require __DIR__ . './../layouts/parts/contents/DocumentalExcel.php';
         $spreadsheet = $reader->loadFromString(ob_get_clean());
         $filename = $reportName . ".xls";
         $filePath = fopen(__DIR__ . DIRECTORY_SEPARATOR . $filename, 'w');
         $writer = IOFactory::createWriter($spreadsheet, 'Xls');
         $writer->save($filePath);
+        $file_with_path = $_SERVER['DOCUMENT_ROOT'] . "/protected/application/plugins/Report/lib/" . $filename;
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="' . $filename . '"');
         header('Cache-Control: max-age=0');
         $writer->save('php://output');
-        unlink($filePath);
+        unlink($file_with_path);
     }
 }
